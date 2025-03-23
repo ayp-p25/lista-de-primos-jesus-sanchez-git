6a15340ea5049c2e70b6e364ae98e554f5d26b95
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-03-23 14:36:42 -0600

    first

--- lista_primos.py
+++ lista_primos.py
@@ -1,15 +1,19 @@
-"""
-Inserta el encabezado aquí y escribe tu código abajo
-"""
-
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+Lim = int(input("Ingresa el limite de la lista : \n"))
+def es_primo(N):
+    P = True
+    i = 2
+    while i < N:
+        if N % i == 0:
+            P = False
+            break
+        i += 1
+    else :
+        P = True
+    return P
+L = []
+N = 2
+while N <= Lim:
+    if es_primo(N):
+        L.append(N)
+    N += 1
+print(L)
